c8e956e26e0b0c9b38895d9aecf89b6208de316a
Danny Hadley <dadleyy@gmail.com>
2017-02-17 13:39:22 -0500

    removing all listing related stuff

--- api/server.js
+++ api/server.js
@@ -4,21 +4,23 @@ const dotenv  = require("dotenv");
 const url     = require("url");
 const qs      = require("querystring");
 
-const API_URL = "https://openapi.etsy.com";
-
 dotenv.load();
 
+const API_URL       = process.env['API_URL'];
+const CLIENT_ID     = process.env["API_CLIENT_ID"];
+const CLIENT_SECRET = process.env["API_CLIENT_SECRET"];
+
 function forwardPath(req, res) {
-  let {pathname, query} = url.parse(req.url);
+  let { pathname, query } = url.parse(req.url);
   let params = qs.parse(query);
-  params.api_key = key;
   let result = [pathname, qs.stringify(params)].join("?");
   console.log(`querying ${result}`);
   return result;
 }
 
+console.log(`proxy to ${API_URL}`);
+
 const app  = express();
-const key  = process.env["ETSY_API_KEY"];
 const mw   = proxy(API_URL, {forwardPath});
 const port = process.argv.length === 3 ? process.argv[2] : "8080";
 
--- app/pods/components/listing-table/component.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import layout from 'charcoal/pods/components/listing-table/template';
-
-const Table = Ember.Component.extend({ layout });
-
-export default Table;
