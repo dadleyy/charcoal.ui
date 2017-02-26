35763602d0982b99658d288ec04ad3cd0782926a
Danny Hadley <dadleyy@gmail.com>
2017-02-26 16:23:45 -0500

    allowing users to start game from empty row

--- app/pods/components/game-display-table/component.js
+++ app/pods/components/game-display-table/component.js
@@ -1,12 +1,26 @@
 import Ember from 'ember';
 
+const { inject } = Ember;
+
 const actions = {
 
   start() {
+    const rounds = this.get('rounds');
+    const { delegate } = this;
+    const { game } = delegate;
+    const { id: game_id } = game;
+
+    function success() {
+      const updated = Date.now();
+      delegate.set('state', { updated });
+    }
+
+    return rounds.create({ game_id }).then(success);
   }
 
 };
 
 export default Ember.Component.extend({
+  rounds: inject.service('game-rounds/resource'),
   actions
 });
