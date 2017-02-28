ccd97c845632dc0142b282ca4324cf529b493bb5
Danny Hadley <dadleyy@gmail.com>
2017-02-28 14:44:44 -0500

    [CHUI-10] exploring server-driven localization definitons

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,3 +1,25 @@
 import Ember from 'ember';
+import ENV from 'charcoal/config/environment';
 
-export default Ember.Route.extend({ });
+const { inject } = Ember;
+const { LOCALE_HOME } = ENV;
+const DEFAULT_LOCALE = 'en';
+
+function model({ locale: url_locale }) {
+  const i18n = this.get('i18n');
+  const locale = url_locale || DEFAULT_LOCALE;
+
+  function success(response) {
+    i18n.addTranslations(locale, response);
+    i18n.set('locale', locale);
+    return true;
+  }
+
+  return this.get('ajax').request(`${LOCALE_HOME}/${locale}`).then(success);
+}
+
+export default Ember.Route.extend({
+  queryParams: { locale: 'en' },
+  i18n: inject.service(), 
+  model
+});
