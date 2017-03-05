0f9b2e9923617cc9fc58e063f976361152c75459
Danny Hadley <dadleyy@gmail.com>
2017-03-05 09:27:47 -0500

    [CHUI-30] hiding new round button to lonely games

--- app/pods/games/single/controller.js
+++ app/pods/games/single/controller.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 const { computed, inject, Controller } = Ember;
+const ACTIVE_STATUS = 'ACTIVE';
 
 function init() {
   this._super(...arguments);
@@ -29,9 +30,19 @@ const lonely = computed('model.manager.state.game.{population}', function() {
   return !population || population < 3;
 });
 
+const active = computed('lonely', 'model.manager.state.game.{status}', function() {
+  const { status } = this.get('model.manager.state.game') || { };
+  return this.get('lonely') === false && status === ACTIVE_STATUS;
+});
+
+const ended = computed('model.manager.state.game.{status}', function() {
+  const { status } = this.get('model.manager.state.game') || { };
+  return status !== ACTIVE_STATUS;
+});
+
 export default Controller.extend({
   users: inject.service('users/resource'),
   memberships: inject.service('game-memberships/resource'),
   rounds: inject.service('game-rounds/resource'),
-  init, actions, lonely
+  init, actions, lonely, active
 });
